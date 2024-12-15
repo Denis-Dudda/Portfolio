e04faf06dc0c3b6b6a430c67c4538126a35dc85e
Denis-Dudda <denis.d@hotmail.de>
2024-12-15 18:21:33 +0100

    add social-container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
 import { NavComponent } from './shared/nav/nav.component';
+import { SocialIconsComponent } from './shared/social-icons/social-icons.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HeroComponent,NavComponent],
+  imports: [CommonModule, RouterOutlet,HeroComponent,NavComponent,SocialIconsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
