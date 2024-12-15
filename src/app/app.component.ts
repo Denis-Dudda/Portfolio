fa6fb4fee209358cec1a241c4f0c935ec88d5e03
Denis-Dudda <denis.d@hotmail.de>
2024-12-15 17:00:42 +0100

    work on hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { NavComponent } from './nav/nav.component';
 import { HeroComponent } from './hero/hero.component';
+import { NavComponent } from './shared/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,NavComponent,HeroComponent],
+  imports: [CommonModule, RouterOutlet,HeroComponent,NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
