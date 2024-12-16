b57ca2dbb07e23a52ea7a68fa08ac1d6045d7ac9
Denis-Dudda <denis.d@hotmail.de>
2024-12-16 10:38:07 +0100

    bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,18 @@ import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
 import { NavComponent } from './shared/nav/nav.component';
 import { SocialIconsComponent } from './shared/social-icons/social-icons.component';
+import { WhyMeComponent } from './why-me/why-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HeroComponent,NavComponent,SocialIconsComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeroComponent,
+    NavComponent,
+    SocialIconsComponent,
+    WhyMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
