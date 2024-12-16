105de6b5325fbfb4a2a946a0a25930194c553beb
Denis-Dudda <denis.d@hotmail.de>
2024-12-16 15:45:30 +0100

    add scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
@@ -17,10 +17,30 @@ import { MySkillSetComponent } from './my-skill-set/my-skill-set.component';
     NavComponent,
     SocialIconsComponent,
     WhyMeComponent,
-    MySkillSetComponent],
+    MySkillSetComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'portfolio';
+
+  ngAfterViewInit(): void {
+    // Event Listener für das horizontale Scrollen mit passive: false hinzufügen
+    window.addEventListener('wheel', (event: WheelEvent) => {
+      event.preventDefault(); // Verhindert das Standard-Scroll-Verhalten (vertikal)
+
+      // Horizontales Scrollen basierend auf der Delta-Werte des Mausrads
+      window.scrollBy({
+        left: event.deltaY < 0 ? -500 : 500, // Scrollt nach links oder rechts, je nach Richtung des Mausrads
+        behavior: 'smooth', // Sanftes Scrollen
+      });
+    }, { passive: false }); // passive auf false setzen wegen neuem Browserschutz
+  }
 }
+
+
+
+
+
+
