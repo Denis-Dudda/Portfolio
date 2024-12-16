e995454fb06abc8916062fedaa95ab02d45af727
Denis-Dudda <denis.d@hotmail.de>
2024-12-16 15:22:37 +0100

    work on myskill

--- src/app/my-skill-set/my-skill-set.component.ts
+++ src/app/my-skill-set/my-skill-set.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-my-skill-set',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-skill-set.component.html',
   styleUrl: './my-skill-set.component.scss'
 })
 export class MySkillSetComponent {
+
 
 }
