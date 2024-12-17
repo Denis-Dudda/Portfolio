5376bf29d1abf51f9da9a8cefc4cf6a0d1407f06
Denis-Dudda <denis.d@hotmail.de>
2024-12-17 21:18:08 +0100

    add card json

--- src/app/my-work/card/card.component.ts
+++ src/app/my-work/card/card.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, } from '@angular/core';
+
+export interface Work {
+  projektNumber: string;
+  projektName: string;
+  aboutTheProjekt: string;
+  technologies: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-
+  @Input() work?: Work;
 }
--- src/app/my-work/my-work.component.ts
+++ src/app/my-work/my-work.component.ts
@@ -10,5 +10,27 @@ import { CardComponent } from './card/card.component';
   styleUrl: './my-work.component.scss'
 })
 export class MyWorkComponent {
-
+  works = [
+    {
+      projektNumber: '1',
+      projektName: 'Join',
+      aboutTheProjekt: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+      technologies: 'JavaScript,HTML,CSS,Firebase',
+      description: 'OR my group-projects experience'
+    },
+    {
+      projektNumber: '2',
+      projektName: 'El Pollo Loco',
+      aboutTheProjekt: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      technologies: 'JavaScript,HTML,CSS,Firebase',
+      description: 'wurst mit dosenkohl'
+    },    
+    {
+      projektNumber: '3',
+      projektName: 'Da Bubble',
+      aboutTheProjekt: 'Posuere pretium arcu sed senectus felis ac mauris.  Dui eget dictum.osuere pretium arcu sed senectus felis ac mauris. h',
+      technologies: 'Angular,TypeScript,JavaScript,HTML,CSS,Firebase',
+      description: 'bla bla bla'
+    },
+  ];
 }
