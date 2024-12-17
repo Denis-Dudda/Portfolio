e8c9dd272a6df824ac88b04b12d27cd8d1f1094a
Denis-Dudda <denis.d@hotmail.de>
2024-12-17 11:06:42 +0100

    work on my skill set

--- src/app/my-skill-set/my-skill-set.component.ts
+++ src/app/my-skill-set/my-skill-set.component.ts
@@ -15,15 +15,15 @@ export class MySkillSetComponent {
 
   icons = [
     { src: 'assets/icons/Property 1.svg', label: 'Icon 1', property: 'Angular' },
-    { src: 'assets/icons/Property 2.svg', label: 'Icon 2', property: 'Angular' },
-    { src: 'assets/icons/Property 3.svg', label: 'Icon 3', property: 'Angular' },
-    { src: 'assets/icons/Property 4.svg', label: 'Icon 4', property: 'Angular' },
-    { src: 'assets/icons/Property 5.svg', label: 'Icon 5', property: 'Angular' },
-    { src: 'assets/icons/Property 6.svg', label: 'Icon 6', property: 'Angular' },
-    { src: 'assets/icons/Property 7.svg', label: 'Icon 7', property: 'Angular' },
-    { src: 'assets/icons/Property 8.svg', label: 'Icon 8', property: 'Angular' },
-    { src: 'assets/icons/Property 9.svg', label: 'Icon 9', property: 'Angular' },
-    { src: 'assets/icons/Property 10.svg', label: 'Icon 10', property: 'Angular' },
-    { src: 'assets/icons/Property 11.svg', label: 'Icon 11', property: 'Angular' }
+    { src: 'assets/icons/Property 2.svg', label: 'Icon 2', property: 'TypeScript' },
+    { src: 'assets/icons/Property 3.svg', label: 'Icon 3', property: 'JavaScript' },
+    { src: 'assets/icons/Property 4.svg', label: 'Icon 4', property: 'HTML' },
+    { src: 'assets/icons/Property 5.svg', label: 'Icon 5', property: 'CSS' },
+    { src: 'assets/icons/Property 6.svg', label: 'Icon 6', property: 'Rest-Api' },
+    { src: 'assets/icons/Property 7.svg', label: 'Icon 7', property: 'Firebase' },
+    { src: 'assets/icons/Property 8.svg', label: 'Icon 8', property: 'Git' },
+    { src: 'assets/icons/Property 9.svg', label: 'Icon 9', property: 'Scrum' },
+    { src: 'assets/icons/Property 10.svg', label: 'Icon 10', property: 'Material design' },
+    { src: 'assets/icons/Property 11.svg', label: 'Icon 11', property: 'Challenge me' }
   ];
 }
